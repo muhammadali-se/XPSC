0f874f5e3e593fce15706bfae1e5a93d27e5645a
Muhammad Ali <114309132+muhammadali-se@users.noreply.github.com>
2024-12-13 00:11:42 +0600

    Update increasingOR.cpp

--- Week-7/Day-5/increasingOR.cpp
+++ Week-7/Day-5/increasingOR.cpp
@@ -3,28 +3,28 @@ using namespace std;
 #define nl '\n'
 
 void solution() {
-    int n; cin >> n;
-	deque<int> ans;
-	
-	for (int k = 0; k <= __lg(n); k++) {
-	    if ((n >> k) & 1) {
-	        long long val = n - (1LL << k);
-	        if (val > 0) ans.push_front(val);
-	    }
-	}
-	
-	ans.push_back(n);
-	cout << ans.size() << nl;
-	for (int val: ans) cout << val << ' ';
-	cout << nl;
+    long long n; cin >> n;
+    deque<long long> ans;
+    
+    for (long long k = 0; k <= __lg(n); k++) {
+        if ((n >> k) & 1) {
+            long long val = n - (1LL << k);
+            if (val > 0) ans.push_front(val);
+        }
+    }
+    
+    ans.push_back(n);
+    cout << ans.size() << nl;
+    for (auto val: ans) cout << val << ' ';
+    cout << nl;
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	
-	int t; cin >> t;
-	while (t--) solution();
-	
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
+    while (t--) solution();
+
+    return 0;
 }
